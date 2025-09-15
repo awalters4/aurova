ba132e67ba64b474573b55a6ab9da2f83c4fccd8
Ariel Walters <arieldwalters4@gmail.com>
2025-09-15 17:58:56 -0500

    stashing old code for now

--- components/MilestoneToast.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import toast from 'react-hot-toast';
-
-export function pop(msg: string) {
-  toast.success(msg, { duration: 3500 });
-}
-
-export default function MilestoneToaster() { return <>{/* toaster outlet */}</>; }
