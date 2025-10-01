66b018c48fc36d6d36b9a22fa84771733465cbd8
Ariel Walters <arieldwalters4@gmail.com>
2025-10-01 12:27:13 -0500

    chore: stupid extra lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
- import { useEffect, useState } from 'react';                                                                        │ │
-import AurovaLanding from "./components/AurovaLanding";                                                             │ │
-import AuthRedirect from "./components/AuthRedirect";                                                      │ │
-                                                                                                                    │ │
-function App() {                                                                                                    │ │
-  const [isAuthRedirect, setIsAuthRedirect] = useState(false);                                                      │ │
-                                                                                                                    │ │
-  useEffect(() => {                                                                                                 │ │
-    // Check if current path is /auth-redirect                                                                      │ │
-    if (window.location.pathname === '/auth-redirect') {                                                            │ │
-      setIsAuthRedirect(true);                                                                                      │ │
-    }                                                                                                               │ │
-  }, []);                                                                                                           │ │
-                                                                                                                    │ │
-  if (isAuthRedirect) {                                                                                             │ │
-    return <AuthRedirect />;                                                                                        │ │
-  }                                                                                                                 │ │
-                                                                                                                    │ │
-  return <AurovaLanding />;                                                                                         │ │
-}                                                                                                                   │ │
-                                                                                                                    │ │
-export default App;
+import { useEffect, useState } from 'react';
+  import AurovaLanding from "./components/AurovaLanding";
+  import AuthRedirect from "./components/AuthRedirect";
+
+  function App() {
+    const [isAuthRedirect, setIsAuthRedirect] = useState(false);
+
+    useEffect(() => {
+      if (window.location.pathname === '/auth-redirect') {
+        setIsAuthRedirect(true);
+      }
+    }, []);
+
+    if (isAuthRedirect) {
+      return <AuthRedirect />;
+    }
+
+    return <AurovaLanding />;
+  }
+
+  export default App;
